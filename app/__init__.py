09d8ca2dce701833cb6cffd6ddcd95bc4b28ce79
Eliot <42266703+smorad3363@users.noreply.github.com>
2026-08-16 17:24:50 +0330

    fix: enforce Unix line endings for shell scripts

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ from fastapi.routing import APIRoute
 
 from config import ALLOWED_ORIGINS, DOCS, XRAY_SUBSCRIPTION_PATH
 
-__version__ = "4.6.1"
+__version__ = "4.6.2"
 
 app = FastAPI(
     title="Network Control API",
